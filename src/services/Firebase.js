b22cafe65ec55987dc312fd1f6e2df6d4d71fbff
hitisha-goyani <goyanihitisha@gmail.com>
2025-08-15 23:38:22 +0530

    set key

--- src/services/Firebase.js
+++ src/services/Firebase.js
@@ -3,13 +3,16 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDT-FhKMVw2_u5rE-cpcv_KaNleSoReQkg",
-  authDomain: "fir-crud-55a2c.firebaseapp.com",
-  projectId: "fir-crud-55a2c",
-  storageBucket: "fir-crud-55a2c.firebasestorage.app",
-  messagingSenderId: "990887184966",
-  appId: "1:990887184966:web:314d048145a2f78beb9847"
+  apiKey: import.meta.env.VITE_FB_API_KEY,
+  authDomain: import.meta.env.VITE_FB_AUTH,
+  projectId: import.meta.env.VITE_FB_PROJECT,
+  storageBucket: import.meta.env.VITE_FB_STORAGE,
+  messagingSenderId: import.meta.env.VITE_FB_MESSAGING,
+  appId: import.meta.env.VITE_FB_API_ID
 };
+
+console.log("apikey",import.meta.env.VITE_FB_API_KEY)
+console.log("authDomain",import.meta.env.VITE_FB_AUTH)
 
 
 const app = initializeApp(firebaseConfig);
